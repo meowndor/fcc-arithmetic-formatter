d0aee0ca61dcdf46d0b4d380439c21c23d2c337b
nuluh <dam.ar@outlook.com>
2022-08-03 21:55:22 +0700

    Added regex for finding the operator

--- arithmetic_formatter.py
+++ arithmetic_formatter.py
@@ -1,15 +1,28 @@
 import re
 
+#regex for finding operator
+def operator(x):
+    if bool(re.search("\+", x)):
+        return "+";
+    elif bool(re.search("\-", x)):
+        return "-"
+
+
+
 def arithmetic_arranger(problems, showanswers=False):
+
+    #check if number of problems 
     if len(problems) > 5:
         print("Error: Too many problems")
+        print("Number of problems should have less than 5")
         exit()
 
-    #check if there's digits for more than 4
-    for i in problems: #problems is 'list' type
-        splitted = i.split("+")
-        
 
+
+    #check if there's more than 4 digits
+    for i in problems: #problems is in 'list' type
+        splitted = i.split(operator(i))
+        
         #traceback error for more than 4 digits
         if len(splitted[0]) > 4:
             print(splitted[0],"\nError: Numbers cannot be more than four digits")
@@ -17,12 +30,16 @@ def arithmetic_arranger(problems, showanswers=False):
 
         elif len(splitted[1]) > 4:
             print(splitted[1], "\nError: Numbers cannot be more than four digits")
-            exit()fdf
+            exit()
 
-    for i in problems: #problems is 'list' type
-        splitted = i.split("+")
 
-        a = list(map(int,splitted)) #turn every splitted items into integer then make it list in pair
+
+
+     #turn every splitted items into integer then make it list in pair
+    for i in problems: #problems is in 'list' type
+        splitted = i.split(operator(i))
+
+        a = list(map(int, splitted))
         print(a)
 
-arithmetic_arranger(["1334+3222","8287+234","2334+56","234+23"])
+arithmetic_arranger(["1334+3222","8287-234","2334+56","234+23"])
