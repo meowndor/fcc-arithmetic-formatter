bfa46cbdb96698c9fbcc4277db760e5af83ddfcf
nuluh <dam.ar@outlook.com>
2022-08-03 17:40:09 +0700

    third commit

--- arithmetic_formatter.py
+++ arithmetic_formatter.py
@@ -1,4 +1,3 @@
-from prompt_toolkit import prompt
 import re
 
 def arithmetic_arranger(problems, showanswers=False):
@@ -6,21 +5,24 @@ def arithmetic_arranger(problems, showanswers=False):
         print("Error: Too many problems")
         exit()
 
-    #split
-    for i in problems:
+    #check if there's digits for more than 4
+    for i in problems: #problems is 'list' type
+        splitted = i.split("+")
+        
+
+        #traceback error for more than 4 digits
+        if len(splitted[0]) > 4:
+            print(splitted[0],"\nError: Numbers cannot be more than four digits")
+            exit()
+
+        elif len(splitted[1]) > 4:
+            print(splitted[1],"\nError: Numbers cannot be more than four digits")
+            exit()
+
+    for i in problems: #problems is 'list' type
         splitted = i.split("+")
 
-        for a in splitted:
+        a = list(map(int,splitted)) #turn every splitted items into integer then make it list in pair
+        print(a)
 
-            b = int(a)
-            #tracaback error for more than 4 digits
-            if len(str(b)) > 4:
-                print(b,"\nError: Numbers cannot be more than four digits")
-                exit()
-            #turn into integer
-            
-
-
-            print(b)
-
-arithmetic_arranger(["1334+34","8234+234","234+56","234+23"])
+arithmetic_arranger(["1334+3222","8287+234","2334+56","234+23"])
